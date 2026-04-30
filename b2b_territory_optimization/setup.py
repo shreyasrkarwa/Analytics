6954f3194cbfbcccd62ab149f3d7ee9acc0e79eb
Shreyas Karwa <shreyasrkarwa@Shreyass-MacBook-Pro.local>
2026-04-30 15:08:47 -0700

    chore: bump version to 0.1.1 for pypi release

--- b2b_territory_optimization/setup.py
+++ b2b_territory_optimization/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="b2b-territory-optimization",
-    version="0.1.0",
+    version="0.1.1",
     author="Shreyas Karwa",
     author_email="",
     description="Mathematical territory carving and dynamic seller allocation for B2B RevOps.",
